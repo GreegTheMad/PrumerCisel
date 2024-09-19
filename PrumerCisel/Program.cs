7a66da66aa2b7667e51fe4e9c2122b0a0097579e
GreegTheMad <164855816+GreegTheMad@users.noreply.github.com>
2024-09-19 07:47:08 +0200

    Initial commit

---
+++ PrumerCisel/Program.cs
@@ -0,0 +1,26 @@
+﻿string val;
+int res;
+bool stop = false;
+List<int> cisla = new List<int>();
+
+while (stop == false)
+    {
+    Console.WriteLine("Dej mi cislo (Zadej -STOP- pro výsledek ");
+    val = Console.ReadLine();
+
+    if (val == "STOP")
+    {
+       stop = true;
+       break;
+    }
+    res = Convert.ToInt32(val);
+    cisla.Add(res);
+}
+
+if (stop == true)
+{
+    double avg = Queryable.Average(cisla.AsQueryable());
+    Console.WriteLine("Průměr = " + avg);
+}
+
+Console.ReadKey();
